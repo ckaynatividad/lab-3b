1e7f45367e1c5331f079fe845fcc0670a4792c1c
ckaynatividad <natividad.ckay@gmail.com>
2021-09-29 15:40:30 -0700

    adding sounds ot img

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const horseImg = document.getElementById('horse');
 const horseSound = document.getElementById('horse-sound');
-
+const dogImg = document.getElementById('dog');
+const catImg = document.getElementById('cat');
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
 
 
 horseImg.addEventListener('click', ()=>{
   horseSound.play();
+});
+
+dogImg.addEventListener('click', ()=>{
+  dogSound.play();
+});
+
+catImg.addEventListener('click', ()=>{
+  catSound.play();
 });
