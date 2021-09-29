5a4f1a5f4cf5f60b0643e587851bdeeae62119da
ckaynatividad <natividad.ckay@gmail.com>
2021-09-29 15:34:08 -0700

    adding horse cat dog audio img

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// import functions and grab DOM elements
+const horseImg = document.getElementById('horse');
+const horseSound = document.getElementById('horse-sound');
 
-// initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+horseImg.addEventListener('click', ()=>{
+  horseSound.play();
+});
