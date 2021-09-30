2349a1c893534a990416b7c3676ba84d9bcbede2
ckaynatividad <natividad.ckay@gmail.com>
2021-09-29 17:11:33 -0700

    adding animation and event keys

--- app.js
+++ app.js
@@ -8,12 +8,32 @@ const catSound = document.getElementById('cat-sound');
 
 horseImg.addEventListener('click', ()=>{
   horseSound.play();
+  horseImg.classList.add('selected');
 });
 
 dogImg.addEventListener('click', ()=>{
-  dogSound.play();
+    dogImg.classList.add('selected');
+    dogSound.play();
+    
 });
 
 catImg.addEventListener('click', ()=>{
   catSound.play();
+  catImg.classList.add('selected');
+});
+
+document.addEventListener('keypress', event =>{
+  if (event.key === 'h') {
+    horseSound.play();
+    horseImg.classList.add('selected');
+  } else if
+    (event.key === 'd') {
+      dogSound.play();
+      dogImg.classList.add('selected');
+    } else if
+    (event.key === 'c') {
+      catSound.play();
+      catImg.classList.add('selected');
+    }
+
 });
